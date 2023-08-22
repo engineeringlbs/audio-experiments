5ce2c7ffd95ccf699c4689d39515e89fb310b24e
builtbyedgar <eledgaar@gmail.com>
2023-08-22 16:38:56 +0200

    feat: playing noises

--- 07-waves/index.js
+++ 07-waves/index.js
@@ -1,3 +1,10 @@
+/**
+ * Based on Fourier transforms
+ *
+ * Refs:
+ * https://stackoverflow.com/questions/1073606/is-there-a-one-line-function-that-generates-a-triangle-wave#answer-1073634
+ */
+
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 const dpxr = window.devicePixelRatio
@@ -8,32 +15,25 @@ const center = height / 2
 const limits = { x: width, y: height - 40 } // Implement limit Y
 
 const parameters = [
-  // { freq: 440, amp: 140, type: 'sine' },
-  // { freq: 3, amp: 25, type: 'sine' },
-  // { freq: 9, amp: 5, type: 'sine' },
-  // { freq: 27, amp: 1, type: 'sine' },
-  // { freq: 81, amp: 1, type: 'noise' },
-  // { freq: 440, amp: 140, type: 'square' },
-  // { freq: 3, amp: 25, type: 'square' },
-  // { freq: 9, amp: 5, type: 'square' },
-  // { freq: 27, amp: 1, type: 'square' },
-  // { freq: 81, amp: 2, type: 'noise' },
-  // { freq: 440, amp: 140, type: 'sawtooth' },
-  // { freq: 3, amp: 25, type: 'sawtooth' },
-  // { freq: 9, amp: 5, type: 'sawtooth' },
-  // { freq: 27, amp: 1, type: 'sawtooth' },
-  { freq: 440, amp: 140, type: 'triangle' },
-  // { freq: 81, amp: 180, type: 'noise' },
+  // freq = x (20hz, 400hz, 1600hz, 20.000hz), amp = y (0, 120)
+  { freq: 440, amp: 120, type: 'sine' },
+  // { freq: 40, amp: 20, type: 'noise' },
+  // { freq: 440, amp: 120, type: 'triangle' },
+  // { freq: 440, amp: 120, type: 'sawtooth' },
+  // { freq: 440, amp: 20, type: 'square' },
+  // { freq: 300, amp: 50, type: 'sine' },
+  // { freq: 150, amp: 10, type: 'sine' },
+  // { freq: 70, amp: 10, type: 'sine' },
 ]
 //
 const zoom = 1 // from 1 to 100?
 const scale = { x: width * zoom, y: 1 }
 
 let currentX = 0
-let lastpos = { x: 0, y: center }
 let wavecolor = '#fefefe'
 let areacolor = 'rgba(255, 255, 255, 0.13)'
 let data = []
+let period = 0.143
 
 canvas.width = width
 canvas.height = height
@@ -56,8 +56,6 @@ function createControls() {
     const label = document.createElement('p')
     const out = document.createElement('span')
     const slider = document.createElement('input')
-    // const min = param
-    console.log(param)
 
     out.setAttribute('class', 'output')
     label.setAttribute('class', 'label flex mb-8')
@@ -77,7 +75,6 @@ createControls()
 // calculate waves
 function update() {
   ctx.clearRect(0, 0, width, height)
-  // caculate wave
   data = []
 
   for (let currentX = 0; currentX < width; currentX++) {
@@ -85,34 +82,40 @@ function update() {
 
     for (let pid = 0; pid < parameters.length; pid++) {
       const p = parameters[pid]
-      const freq = p.freq / 20
-      // https://stackoverflow.com/questions/1073606/is-there-a-one-line-function-that-generates-a-triangle-wave#answer-1073634
-      // sine wave
+      const freq = p.freq * period
+      // ✅ Sine wave
       const v = Math.sin((freq * currentX) / scale.x)
       let value = v
 
-      // squarepusher wave
-      if (p.type == 'squarepusher') {
-        value = currentY - Math.floor(v + 1 / 2)
-      }
-      // sawquare wave
-      if (p.type == 'sawquare') {
-        value = v - Math.floor(v + 1 / 2)
-      }
-      // sawtooth wave
-      if (p.type == 'sawtooth') {
-        // value = 1 - 2 * ((scale.x / currentX) % 1) // mola
-        value = 1 - 2 * ((currentX / scale.x) % 1)
-      }
-      // trangle wave
+      // ✅ Triangle wave
       if (p.type == 'triangle') {
-        value = freq - ((currentX / scale.x) % freq) - freq
+        const rel = scale.x / (freq * period)
+        const t = currentX % rel
+
+        if (t < rel / 2) {
+          value = t / (rel / 2)
+        } else {
+          value = (rel - t) / (rel / 2)
+        }
+
+        value = 2 * value - 1
       }
-      // square wave
+
+      // ✅ Sawtooth wave
+      if (p.type == 'sawtooth') {
+        const t = currentX / (scale.x / (freq * period))
+        const w = t - Math.floor(t)
+
+        // Note thach switching 2 for -2 the waveform direction changes
+        value = 2 * (w - 0.5)
+      }
+
+      // ✅ Square wave
       if (p.type == 'square') {
-        value = v >= 0 ? 1 : -1
+        value = value >= 0 ? 1 : -1
       }
-      // noise wave
+
+      // ✅ Noise wave
       if (p.type == 'noise') {
         value = Math.random() - 0.5
       }
@@ -144,4 +147,112 @@ function drawWave(ctx, data) {
   ctx.strokeStyle = wavecolor
   ctx.lineWidth = 1 * dpxr
   ctx.stroke()
+}
+
+// ✅ Squarepusher wave
+// if (p.type == 'squarepusher') {
+//   value = currentY - Math.floor(value + 1 / 2)
+// }
+// ✅ Sawquare wave
+// if (p.type == 'sawquare') {
+//   value = value - Math.floor(value + 1 / 2)
+// }
+
+/**
+ * Let's try to play the waveforms sounds
+ */
+const playBtn = document.querySelector('#play')
+playBtn.addEventListener('click', play, false)
+
+const acontext = new AudioContext()
+
+function play() {
+  const now = acontext.currentTime
+  const pdata = data.map((d) => d / 1000)
+  console.log(pdata)
+
+  const ft = new DFT(pdata.length)
+  ft.forward(pdata)
+  console.log(ft)
+
+  const osc = new OscillatorNode(acontext)
+  const wave = acontext.createPeriodicWave(ft.real, ft.imag)
+
+  osc.setPeriodicWave(wave)
+  osc.connect(acontext.destination)
+
+  osc.start(now)
+  osc.stop(now + 2)
+
+  osc.onended = () => {
+    osc.disconnect()
+  }
+}
+
+// DFT is a class for calculating the Discrete Fourier Transform of a signal.
+function DFT(bufferSize, sampleRate) {
+  const N = (bufferSize / 2) * bufferSize
+  const TWO_PI = 2 * Math.PI
+
+  this.bufferSize = bufferSize
+  this.sampleRate = sampleRate
+  this.bandwidth = ((2 / bufferSize) * sampleRate) / 2
+
+  this.spectrum = new Float64Array(bufferSize / 2)
+  this.real = new Float64Array(bufferSize)
+  this.imag = new Float64Array(bufferSize)
+
+  this.peakBand = 0
+  this.peak = 0
+
+  this.sinTable = new Float64Array(N)
+  this.cosTable = new Float64Array(N)
+
+  for (let i = 0; i < N; i++) {
+    this.sinTable[i] = Math.sin((i * TWO_PI) / bufferSize)
+    this.cosTable[i] = Math.cos((i * TWO_PI) / bufferSize)
+  }
+
+  this.forward = (buffer) => {
+    let real = this.real
+    let imag = this.imag
+    let rval = undefined
+    let ival = undefined
+
+    for (let k = 0; k < this.bufferSize / 2; k++) {
+      rval = 0.0
+      ival = 0.0
+
+      for (let n = 0; n < buffer.length; n++) {
+        rval += this.cosTable[k * n] * buffer[n]
+        ival += this.sinTable[k * n] * buffer[n]
+      }
+
+      real[k] = rval
+      imag[k] = ival
+    }
+
+    return this.calculateSpectrum()
+  }
+
+  this.calculateSpectrum = () => {
+    const bSi = 2 / this.bufferSize
+    const sqrt = Math.sqrt
+    let rval
+    let ival
+    let mag
+
+    for (var i = 0, N = this.bufferSize / 2; i < N; i++) {
+      rval = this.real[i]
+      ival = this.imag[i]
+      mag = bSi * sqrt(rval * rval + ival * ival)
+
+      if (mag > this.peak) {
+        this.peakBand = i
+        this.peak = mag
+      }
+
+      this.spectrum[i] = mag
+    }
+  }
 }
